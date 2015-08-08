9804f7d6ff08480a5a2423870dcdde673d36df24
Tom Kadwill <tomkadwill@gmail.com>
2015-08-08 10:51:59 +0100

    Add backup script

---
+++ backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "running 'mackup backup'.."
+mackup backup
+echo "'mackup backup' completed"
+
+echo "backing up /Users/tom/.vim/ .."
+if [ -d ~/Dropbox/Mackup/.vim/ ]; then
+    echo "A file named /Users/tom/Dropbox/Mackup/.vim already exists in the backup."
+    read -p "Are you sure that you want to replace it ? <Yes|No>" -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      cp -r ~/.vim ~/Dropbox/Mackup/.vim
+      rm -rf ~/.vim
+      ln -s ~/Dropbox/Mackup/.vim ~/.vim
+    fi
+fi
+
+exit
